30cb5fb899c75e46476fb3b5d775e6ce7277df94
adjash <adam.hardie13@gmail.com>
2025-07-03 18:46:46 +0100

    base url change

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "https://adjash.github.io/",
+  base: "https://adjash.github.io/beat-app",
   plugins: [react()],
 });
